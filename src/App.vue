fef28efb42a2efd30cbed11e18589c326af86dee
FHangH <752972182@qq.com>
2024-05-22 16:31:57 +0800

    在Vue3使用Vue2

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
 <template>
     <div class="app">
         <h1>Hello World</h1>
+        <Person></Person>
     </div>
 </template>
 
 <script lang="ts">
+    import Person from './components/Person.vue';
+
     export default 
     {
-        name: 'App' // 组件名称，可选，但建议填写，方便调试时识别组件
+        name: 'App', // 组件名称，可选，但建议填写，方便调试时识别组件
+        components: { Person }, // 引入组件，可选，但建议填写，方便调试时识别组件
     }
 </script>
 
-<style>
+<style>s
     .app
     {
         background-color: #f0f0f0;
---
+++ src/components/Person.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="Person">
+        <h2>Name: {{name}}</h2>
+        <h2>Age: {{age}}</h2>
+        <button @click="ChangeName">ChangeName</button>
+        <button @click="ChangeAge">ChangeAge</button>
+        <button @click="ShowTel">Show Telephone</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default 
+    {
+        name: 'PersonInfo',
+
+        data(): any
+        {
+            return { name: 'John', age: 30, tel: '123456789' } // 数据属性
+        },
+
+        methods: // 方法属性
+        {
+            ShowTel(): void // 方法定义
+            {
+                alert(this.tel); // 方法体
+            },
+
+            ChangeName(): void
+            {
+                this.name = 'Tom';
+            },
+
+            ChangeAge(): void
+            {
+                this.age += 1;
+            },
+        }
+    };
+</script>
+
+<style scoped>
+    .Person
+    {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    button
+    {
+        margin-right: 5px;
+    }
+</style>
