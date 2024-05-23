e80eb2c666616c55c44b43bbbf3573d5a975b1b7
FHangH <752972182@qq.com>
2024-05-23 09:15:14 +0800

    ref 和 reactive 创建响应式对象的区别

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,6 +2,7 @@
     <div class="Person">
         <h2>{{car.brand}} Price: {{car.price}}</h2>
         <button @click="ChangePrice">ChangePrice</button>
+        <button @click="ChangeCar">ChangeCar</button>
 
         <hr>
         <h2>GameList:</h2>
@@ -13,9 +14,14 @@
 </template>
 
 <script lang="ts" setup name="PersonInfo">
-    import { ref } from 'vue';
+    import { ref, reactive } from 'vue';
 
+    // let car = reactive({brand: 'VW', price: 100});
     let car = ref({brand: 'VW', price: 100});
+
+    // 基本数据响应：ref
+    // 对象响应（对象层级简单）：ref，reactive 都可以，推荐 ref，因为 ref 更简单，不需要写 .value 访问属性。
+    // 对象层级复杂：reactive，因为 reactive 支持对象层级响应，ref 则不支持。
 
     let games = ref
     ([
@@ -31,6 +37,16 @@
     function ChangePrice()
     {
         car.value.price += 100;
+    }
+
+    function ChangeCar()
+    {
+        // car = reactive({brand: 'BMW', price: 200});
+        // reactive 重分配对象后，将不再是响应式数据
+        //Object.assign(car, {brand: 'BMW', price: 200});
+
+        // ref 重分配后，可以正常响应数据
+        car.value = {brand: 'BMW', price: 200};
     }
 
     function RenameFirstGameName()
