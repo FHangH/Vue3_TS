505e567bf99f58e0a50ac71ede8eed11e3632a4c
FHangH <752972182@qq.com>
2024-05-24 15:29:51 +0800

    defineProps, withDefaults

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 // template: 结构(html) script: 逻辑(js/ts) style: 样式
 
 <template>
-    <Person/>
+    <Person :list="personList"/>
 </template>
 
 <script lang="ts" setup name="App">
     import Person from './components/Person.vue';
+    import { reactive } from 'vue';
+    import { type PersonList } from '@/types';
+
+    let personList = reactive<PersonList>
+        ([
+            { id: '123546', name: '张三', age: 18},
+            { id: '123547', name: '李四', age: 20},
+            { id: '123548', name: '王五', age: 22},
+        ]);
+
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,24 +1,37 @@
 <template>
     <div class="Person">
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                {{item.name}} - {{item.age}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts" setup name="PersonInfo">
-    import { type IPerson, type PersonList } from '@/types';
+    import { defineProps, withDefaults } from 'vue';
+    import { type PersonList } from '@/types';
 
-    // let person: IPerson = { id: '123456', name: '张三', age: 30}
-    let personList: PersonList = 
-        [
-            {id: '123456', name: '张三', age: 30},
-            {id: '000000', name: 'lisi', age: 10}
-        ];
+    // 定义组件的属性类型和结构，并使用defineProps进行声明。
+    // defineProps(['list']);
+    
+    // 使用类型断言来指定list的类型。
+    // defineProps<{list: PersonList}>();
+
+    withDefaults
+    (
+        defineProps<{list?: PersonList}>(), 
+        {
+            list: () => [{ id: '123546', name: '张三', age: 18}]
+        }
+    );
 </script>
 
 <style scoped>
     .Person
     {
         background-color: skyblue;
-        box-shadow: 0 0 10px;
+        box-shadow: 0 0 10px;   
         border-radius: 10px;
         padding: 20px;
     }
